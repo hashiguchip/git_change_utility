738878a3abe6fd4fcc753db733337aa21da8cacc
hashiryo <kuttc777+@gmail.com>
2019-02-11 16:32:55 +0900

    [add] copy command to clipboard

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
         section.main-content
             article.article-block
                 h2.title gitのauthor履歴を変更する
+                button.copy-button(v-on:click="copyGitCommand") 下記のgitコマンドをコピーする
                 .body git filter-branch -f --env-filter "GIT_AUTHOR_NAME='{{this.author}}'; GIT_AUTHOR_EMAIL='{{this.email}}'; GIT_COMMITTER_NAME='{{this.committer}}'; GIT_COMMITTER_EMAIL='{{this.email}}';" HEAD
 </template>
 
@@ -26,6 +27,19 @@ export default class extends Vue {
   }
   get email() {
     return this.$store.state.user.email;
+  }
+  private copyGitCommand(event) {
+    const targetCommand = event.target.nextElementSibling.innerHTML;
+    this.copyStrToClipboard(targetCommand);
+  }
+  //todo : move this to common functions
+  public copyStrToClipboard(target: string): void {
+    const ta = document.createElement("textarea");
+    ta.value = target;
+    document.body.appendChild(ta);
+    ta.select();
+    document.execCommand("copy");
+    ta.parentElement.removeChild(ta);
   }
 }
 </script>
